d18d8ee25819e61e71004ab99e9672498ee705ca
Jesus Revilla <jesus_revilla@yahoo.com>
2020-04-17 11:14:08 -0500

    SetUp function simplified

--- test_listcomparator.py
+++ test_listcomparator.py
@@ -5,13 +5,11 @@ from compare_two_lists import ListComparator
 
 class ListComparatorTest(unittest.TestCase):
     def setUp(self):
-        list_01 = ["1", "2", 3]
-        list_02 = ["1", "2", 3]
-        list_03 = []
-        list_04 = ["1", 3, "2"]
-        self.listcomparator = ListComparator(list_01, list_02)
-        self.listcomparator_different = ListComparator(list_01, list_03)
-        self.listcomparator_different_position = ListComparator(list_01, list_04)
+        self.list_01 = ["1", "2", 3]
+        self.list_02 = ["1", "2", 3]
+        self.list_03 = []
+        self.list_04 = ["1", 3, "2"]
+        self.listcomparator = ListComparator(self.list_01, self.list_02)
 
     def test_lists_length(self):
         is_equal_lenth = self.listcomparator.are_list_equal_len()
@@ -22,9 +20,13 @@ class ListComparatorTest(unittest.TestCase):
         self.assertEqual(True, are_elements_equal)
 
     def test_are_elements_not_equal(self):
-        elements_not_equal = self.listcomparator_different.are_elements_equal()
+        listcomparator_different = ListComparator(self.list_01, self.list_03)
+        elements_not_equal = listcomparator_different.are_elements_equal()
         self.assertEqual(False, elements_not_equal)
 
     def test_different_position(self):
+        self.listcomparator_different_position = ListComparator(
+            self.list_01, self.list_04
+        )
         different_position = self.listcomparator_different_position.are_list_equal()
         self.assertEqual(False, different_position)
