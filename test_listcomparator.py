7b93ea5f24f97058ec1ab351af171713ecec388e
Jesus Revilla <jesus_revilla@yahoo.com>
2020-04-17 12:44:45 -0500

    Added empty list test

--- test_listcomparator.py
+++ test_listcomparator.py
@@ -5,28 +5,27 @@ from compare_two_lists import ListComparator
 
 class ListComparatorTest(unittest.TestCase):
     def setUp(self):
-        self.list_01 = ["1", "2", 3]
-        self.list_02 = ["1", "2", 3]
-        self.list_03 = []
-        self.list_04 = ["1", 3, "2"]
-        self.listcomparator = ListComparator(self.list_01, self.list_02)
+        self.listcomparator = ListComparator()
 
     def test_lists_length(self):
-        is_equal_lenth = self.listcomparator.are_list_equal_len()
-        self.assertEqual(True, is_equal_lenth)
+        is_equal_lenth = self.listcomparator.is_equal_len([], [])
+        self.assertTrue(is_equal_lenth)
 
     def test_are_elements_equal(self):
-        are_elements_equal = self.listcomparator.are_elements_equal()
-        self.assertEqual(True, are_elements_equal)
+        are_elements_equal = self.listcomparator.are_elements_equal([1, 2], [1, 2])
+        self.assertTrue(are_elements_equal)
 
     def test_are_elements_not_equal(self):
-        listcomparator_different = ListComparator(self.list_01, self.list_03)
-        elements_not_equal = listcomparator_different.are_elements_equal()
-        self.assertEqual(False, elements_not_equal)
+        elements_not_equal = self.listcomparator.are_elements_equal([], [1])
+        self.assertFalse(elements_not_equal)
 
     def test_different_position(self):
-        self.listcomparator_different_position = ListComparator(
-            self.list_01, self.list_04
-        )
-        different_position = self.listcomparator_different_position.are_list_equal()
-        self.assertEqual(False, different_position)
+        different_position = self.listcomparator.are_list_equal([1, 2], [2, 1])
+        self.assertFalse(different_position)
+
+    def test_empty_lists(self):
+        self.assertTrue(self.listcomparator.are_list_equal([], []))
+
+
+if __name__ == "__main__":
+    unittest.main()
